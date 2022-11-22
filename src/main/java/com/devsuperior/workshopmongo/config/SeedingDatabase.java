0fa3a3f1de1da1b68648a6cc3f4b7d2d808ca694
Alexandre <alexandre.si.ufu@gmail.com>
2022-11-22 17:42:43 -0300

    database seeding

--- src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
+++ src/main/java/com/devsuperior/workshopmongo/config/SeedingDatabase.java
@@ -12,6 +12,9 @@ import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @Configuration
 public class SeedingDatabase implements CommandLineRunner {
 
@@ -23,11 +26,38 @@ public class SeedingDatabase implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Mono<Void> deleteUsers = userRepository.deleteAll();
+		deleteUsers.subscribe();
+		
+		Mono<Void> deletePosts = postRepository.deleteAll();
+		deletePosts.subscribe();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Flux<User> insertUsers = userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		insertUsers.subscribe();	
+		
+		Post post1 = new Post(null, Instant.parse("2022-11-21T18:35:24.00Z"), "Partiu viagem",
+				"Vou viajar para São Paulo. Abraços!", maria.getId(), maria.getName());
+		Post post2 = new Post(null, Instant.parse("2022-11-23T17:30:24.00Z"), "Bom dia", "Acordei feliz hoje!",
+				maria.getId(), maria.getName());
+		
+		post1.addComment("Boa viagem mano!", Instant.parse("2022-11-21T18:52:24.00Z"), alex.getId(), alex.getName());
+		post1.addComment("Aproveite!", Instant.parse("2022-11-22T11:35:24.00Z"), bob.getId(), bob.getName());
 
+		post2.addComment("Tenha um ótimo dia!", Instant.parse("2022-11-23T18:35:24.00Z"), alex.getId(), alex.getName());
+		
+		Flux<Post> insertPosts = postRepository.saveAll(Arrays.asList(post1, post2));
+		insertPosts.subscribe();		
+
+		/*
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User maria = new User(null, "Maria Brown II", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
@@ -47,6 +77,7 @@ public class SeedingDatabase implements CommandLineRunner {
 
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		*/
 	}
 
 }
