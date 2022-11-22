f0d037865e093a50d80742f8bcc2a8236a7cf5e3
Alexandre <alexandre.si.ufu@gmail.com>
2022-11-22 17:16:29 -0300

    including webflux and mongodb reactive, alter repositories

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,23 +1,17 @@
 package com.devsuperior.workshopmongo.services;
 
-import java.time.Instant;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.workshopmongo.dto.PostDTO;
-import com.devsuperior.workshopmongo.entities.Post;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
-import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
 @Service
 public class PostService {
 
 	@Autowired
 	private PostRepository repository;
-
+	
+	/*
 	@Transactional(readOnly = true)
 	public PostDTO findById(String id) {
 		Post post = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
@@ -34,4 +28,5 @@ public class PostService {
 		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
 		return result;
 	}
+	*/
 }
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,16 +1,9 @@
 package com.devsuperior.workshopmongo.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.workshopmongo.dto.PostDTO;
-import com.devsuperior.workshopmongo.dto.UserDTO;
-import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
-import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -18,6 +11,7 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
+	/*
 	@Transactional(readOnly = true)
 	public List<UserDTO> findAll() {
 		List<UserDTO> result = repository.findAll().stream().map(x -> new UserDTO(x)).toList();
@@ -65,4 +59,5 @@ public class UserService {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
 	}
+	*/
 }
